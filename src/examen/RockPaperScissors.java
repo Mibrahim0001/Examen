6c9d178aac79c59b415dc727a385fa6655648fea
Mibrahim0001 <156779985+Mibrahim0001@users.noreply.github.com>
2024-02-26 13:30:53 +0100

    Proyecto documentado

--- src/examen/RockPaperScissors.java
+++ src/examen/RockPaperScissors.java
@@ -1,15 +1,30 @@
 package examen;
 
 import java.util.*;
-
+/**
+ * Clase donde se almacena la logica del juego
+ * 
+ * @author Muhammad Ibrahim
+ * @version 1.0.0
+ */
 public class RockPaperScissors {
+	/**
+	 * Variable donde se almacenan las opciones del juego
+	 */
 	public static final List<String> opcionList = Arrays.asList("Rock", "Paper", "Scissors");
 	private static int keyboard = 0;
 	private static Scanner scanner = new Scanner(System.in);
-
+	
+	/**
+	 * Constructor por defecto de la clase
+	 */
 	public RockPaperScissors() {
 	}
-
+	
+	/**
+	 * Metodo main  de la clase, para iniciar el juego
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		List<String> optionList = opcionList;
 		do {
@@ -24,7 +39,13 @@ public class RockPaperScissors {
 		System.out.println(RockPaperScissors.checkGame(userChoice, computerChoice));
 		scanner.close();
 	}
-
+	
+	/**
+	 * Metodo para comprobar el ganador del partido
+	 * @param userChoice la eleccion del juegador
+	 * @param computerChoice la eleccion de la maquina
+	 * @return Si ha ganado la maquina o el jugador
+	 */
 	public static String checkGame(String userChoice, String computerChoice) {
 		String gameResult = "";
 		if (userChoice.equals(computerChoice)) {
